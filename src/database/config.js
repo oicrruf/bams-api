ca4a289d7bc1204c7e4b948449ae8ea89146b16b
olivia2283 <omunozca@gmail.com>
2023-08-22 20:44:39 -0600

    config

---
+++ src/database/config.js
@@ -0,0 +1,12 @@
+import Sequelize from 'sequelize'
+
+export const sequelize = new Sequelize(
+  'uluqgckh', // nombre de la base de datos 
+  'postgres', // nombre del usuario
+  'lm-Zc0fIms766L3z56Q77ESSuvKWHUnN', // contraseña de la base
+  {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres'
+  }
+)
