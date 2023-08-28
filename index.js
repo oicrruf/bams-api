4bc3103f95bcb98546b6894125a2e4230aea3afa
olivia2283 <omunozca@gmail.com>
2023-08-28 16:31:21 -0600

    CRUD Role

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { sequelize } from "./src/database/config.js"
 import { errorHandler } from './src/middleware/errorMiddleware.js'
 import routeUsers from './src/routes/usersRoutes.js'
 import rutasCondominio from './src/routes/condoRoutes.js'
+import routeRole from './src/routes/roleRoutes.js'
 
 async function main(){
   try{
@@ -16,7 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(routeUsers)
-
+app.use(routeRole)
 app.use(rutasCondominio)
 app.use(errorHandler)
 
