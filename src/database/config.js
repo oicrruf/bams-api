4dce4870827d69d5302c4e9ac0a4719116a694a5
Víctor M. Reyes <victor.reyes@focusservices.com>
2023-08-31 20:12:19 -0600

    chore: db configuration

--- src/database/config.js
+++ src/database/config.js
@@ -1,14 +1,20 @@
-import Sequelize from 'sequelize'
+import Sequelize from "sequelize";
 
- export const sequelize = new Sequelize(
-  'uluqgckh', // nombre de la base de datos 
-  'uluqgckh', // nombre del usuario
-  'RLnFlSUmuWTcWi9UKBzkjIbW2Fqu3HkG', //contraseña de la base de datos
+// export const sequelize = new Sequelize(
+//   process.env.NAME_DB, // nombre de la base de datos
+//   process.env.USER_DB, // nombre del usuario
+//   process.env.PASSWORD_DB, //contraseña de la base de datos
+//   {
+//     host: process.env.HOST_DB,
+//     port: process.env.PORT_DB,
+//     dialect: "postgres",
+//   }
+// );
+export const sequelize = new Sequelize(
+  "postgres://bams:KpQRu2UAsbm0ObySXY67LgiNLJ2DvYAl@dpg-cjojkl36fquc73d6rtdg-a.oregon-postgres.render.com/bams",
   {
-    host: 'mahmud.db.elephantsql.com',
-    port: 5432,
-    dialect: 'postgres'
-  }   
-)
-
-
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: { native: true, ssl: true },
+  }
+);
